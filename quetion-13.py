60611a455c8457aa3cff1320aedd0553f113a9aa
Jogendrakumawat7096 <jogendrakumawat7096@gmail.com>
2023-10-03 11:19:12 +0530

    03-10-2023

--- quetion-13.py
+++ quetion-13.py
@@ -1,13 +1,13 @@
 # Write a Python program to count the number of characters (character frequency) in a string
 
 str1 = input ("Enter the string: ")
-d=dict()
+d1=dict()
 for i in str1:
-    if i in d:
-        d[i] = d[i]+1
+    if i in d1:
+        d1[i] = d1[i]+1
     else:
-        d[i]=1
-print(d)
+        d1[i]=1
+print(d1)
 
 
    
